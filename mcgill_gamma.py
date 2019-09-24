2efb67021b6cf584409bdc40455a22b7b9004275
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-09-24 14:12:22 -0400

    Added pump-off diagnostic format

--- mcgill_gamma.py
+++ mcgill_gamma.py
@@ -186,3 +186,71 @@ class McGillRawDatasetGamma(AbstractRawDataset):
             im -= laser_bg
 
         return im
+
+class McGillRawDatasetGammaPumpoff(McGillRawDatasetGamma):
+    """
+    Diagnostic raw dataset from the Siwick Research Group Diffractometer, in use 
+    starting late 2019. This dataset will reduce only pumpoff pictures
+
+    Parameters
+    ----------
+    source : str
+        Raw data directory
+    
+    Raises
+    ------
+    ValueError : if the source directory does not exist.
+    """
+
+    display_name = "McGill Raw Dataset v. Gamma [Diagnostic pump-off]"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scans = [1]
+
+        # Determine time-stamps from filenames
+        fnames = {k for (k, v) in self.timestamps.items() if k.parent == Path("pump_off")}
+        self.time_points = np.asfarray([self.timestamps[fname] for fname in fnames])
+    
+    @check_raw_bounds
+    def raw_data(self, timedelay, scan=1, bgr=True, **kwargs):
+        """
+        Returns an array of the image at a timedelay and scan. Laser background is
+        removed by default.
+        
+        Parameters
+        ----------
+        timdelay : float
+            Acquisition time-delay.
+        scan : int, optional
+            Scan number. Default is 1.
+        bgr : bool, optional
+            If True (default), the laser background is removed as well.
+        kwargs
+            Extra keyword arguments are ignored.
+        
+        Returns
+        -------
+        arr : `~numpy.ndarray`, ndim 2
+        
+        Raises
+        ------
+        ValueError : if ``timedelay`` or ``scan`` are invalid / out of bounds.
+        IOError : Filename is not associated with an image/does not exist.
+        """
+        # In this case, the time-delay is the pump-off timestamp
+        fname = self.source / "pump_off" / f"pump_off_epoch_{timedelay:010.0f}s.tif"
+
+        if not fname.exists():
+            raise IOError(
+                f"Expected the file for {timedelay}ps and scan {scan} to exist, but could not find it."
+            )
+
+        im = diffread(fname)
+        if bgr:
+            dark_bg = self.nearest_dark(
+                timestamp=self.timestamps[fname.relative_to(self.source)]
+            )
+            im -= dark_bg
+
+        return im
